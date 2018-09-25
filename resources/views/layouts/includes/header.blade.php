e274bf4bdbe33603aa48ea13b687b800b1a605df
rifmeister <chinsanity16@gmail.com>
2018-09-25 08:04:10 +0000

    en-tete

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -55,7 +55,7 @@
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                             <span class="d-none d-md-block">Cr&eacute; <i class="fa fa-angle-down"></i></span>
+                             <span class="d-none d-md-block">Cr&eacute;er <i class="fa fa-angle-down"></i></span>
                              <span class="d-block d-md-none"><i class="fa fa-plus"></i></span>   
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
