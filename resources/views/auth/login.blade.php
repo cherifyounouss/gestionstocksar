437fc3b8536d426eea8d2f15335054c671585a59
rifmeister <chinsanity16@gmail.com>
2018-09-25 08:03:24 +0000

    login view v2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,6 +41,11 @@
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center bg-dark">
             <div class="auth-box bg-dark border-top border-secondary">
                 <div id="loginform">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
                         @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul>
