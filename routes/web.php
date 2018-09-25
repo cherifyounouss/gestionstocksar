b6c3414fa18520ad66f06c5200e5e280627efad1
rifmeister <chinsanity16@gmail.com>
2018-09-25 08:07:16 +0000

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
 
 Auth::routes();
@@ -22,7 +22,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/se_connecter','Admin\LoginController@showLoginForm');
 Route::post('/connexion','Admin\LoginController@login');
 Route::post('Admin\LoginController@logout');
+Route::get('utilisateur/reset/{token}', 'Admin\ResetPasswordController@showResetForm');
+Route::post('utilisateur/reset', 'Admin\ResetPasswordController@reset');
 Route::get('/accueil', function() {
     return "Bienvenue sur la page d'accueil";
 });
 Route::post('/password_request','Admin\ForgotPasswordController@sendResetLinkEmail');
+
+//Fournisseur
+Route::get('/ajouter_fournisseur', 'Admin\ProviderController@create');
+Route::post('/ajouter_fournisseur', 'Admin\ProviderController@store');
+Route::get('/liste_fournisseur', 'Admin\ProviderController@index');
+Route::get('/modifier_fournisseur/{id}', 'Admin\ProviderController@edit');
+Route::post('/modifier_fournisseur/{id}', 'Admin\ProviderController@update');
+Route::post('/supprimer_fournisseur/{id}','Admin\ProviderController@destroy');
