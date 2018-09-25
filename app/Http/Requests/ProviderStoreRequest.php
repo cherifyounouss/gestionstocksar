97fbe2d64ee2f2028144d7156eae0b2c7e37f39c
rifmeister <chinsanity16@gmail.com>
2018-09-25 08:05:40 +0000

    request: controller

---
+++ app/Http/Requests/ProviderStoreRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProviderStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nom_fournisseur' => 'required|min:2',
+            'adresse' => 'required'
+        ];
+    }
+
+     /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+
+    public function messages(){
+        return [
+            'nom_fournisseur.required' => 'Veuillez entrer le nom du fournisseur',
+            'adresse.required' => 'Veuillez entrer l\'adresse du fournisseur',
+            'nom_fournisseur.min' => 'Veuillez entrer un nom valide de fournisseur'
+        ];
+    }
+}
