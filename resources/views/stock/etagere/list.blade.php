2033479e4ed9019a7d3513058a434462e4f57c69
rifmeister <chinsanity16@gmail.com>
2018-09-28 08:08:36 +0000

    view: liste etagere

--- resources/views/stock/etagere/list.blade.php
+++ resources/views/stock/etagere/list.blade.php
@@ -24,15 +24,16 @@
   <div class="card-body">
     @if(Session::has('successMessage'))
         <div class="alert alert-success" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>        
             {{session('successMessage')}}
         </div>        
     @endif
-    <h5 class="card-title">Liste des &eacute;tag&egrave;res</h5>
+    <h5 class="card-title" id="liste_title">Liste des &eacute;tag&egrave;res</h5>
     <div class="table-responsive">
         <table id="table_etagere" class="table table-striped table-bordered">
             <thead>
                 <tr>
-                    <th><b>Libelle &eacute;tag&egravere;</b></th>
+                    <th><b>Libelle &eacute;tag&egrave;re</b></th>
                     <th><b>Nombres de casiers</b></th>
                     <th><b>Actions</b></th>
                 </tr>
@@ -47,15 +48,14 @@
                         <form action="{{url('/stock/modifier_etagere',$etagere->id)}}" style="display: inline;">
                             <button type="submit" class="btn btn-cyan btn-sm">Modifier</button>
                         </form>
-                        <input type="hidden">
-                        <button type="button" class="btn btn-danger btn_delete btn-sm" data-toggle="modal">Supprimer</button>
+                        <button type="button" onclick="supprimer_etagere({{ $etagere->id}})" id="btn_delete" class="btn btn-danger btn_delete btn-sm" data-toggle="modal">Supprimer</button>
                     </td>                  
                 </tr>
                 @endforeach
             </tbody>
             <tfoot>
                 <tr>
-                    <th><b>Libelle &eacute;tag&egravere;</b></th>
+                    <th><b>Libelle &eacute;tag&egrave;re</b></th>
                     <th><b>Nombres de casiers</b></th>
                     <th><b>Actions</b></th>
                 </tr>
@@ -66,9 +66,35 @@
 @section('scripts')
 <script src="{{asset('assets/extra-libs/DataTables/datatables.min.js')}}"></script>
 <script>
+
+    $(document).ready(function(){
+        //On affiche une notification de suppression si la variable de notification de suppression existe
+        if(sessionStorage.getItem('messageSuppression')){
+            $('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+sessionStorage.getItem('messageSuppression')+'</div>').insertBefore($('#liste_title'));
+            sessionStorage.removeItem('messageSuppression');
+        }
+    })    
+
     /****************************************
         *       Basic Table                   *
         ****************************************/
     $('#table_etagere').DataTable();
+
+    function supprimer_etagere(id) {
+        if (confirm("Voulez-vous vraiment supprimer cet etagere...?")) {
+            $.ajax({
+                method: "POST",
+                url: "{{url('/stock/supprimer_etagere')}}",
+                data: {id: id, _token: "{{ csrf_token() }}"}, 
+            }).done(function(response){
+                if(response == "success"){
+                    //On crée une variable afin de contenir la notification de suppression                    
+                    sessionStorage.setItem('messageSuppression', 'L\'étagère a été supprimé');
+                    location.reload(true);
+                }
+            });
+        }
+    }
+    
 </script>
 @endsection
