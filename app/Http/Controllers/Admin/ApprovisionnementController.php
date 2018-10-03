2e0b95a3d6bb8f5cc6cddf4a2b76c32f2305efde
rifmeister <chinsanity16@gmail.com>
2018-10-03 08:12:24 +0000

    controller: ApprovisionnementController

--- app/Http/Controllers/Admin/ApprovisionnementController.php
+++ app/Http/Controllers/Admin/ApprovisionnementController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApprovisionnementRequest;
 use App\Produit;
 use App\Fournisseur;
 use App\Approvisionnement;
 use App\Http\Controllers\Admin\BaseController;
+use DateTime;
 
 
 class ApprovisionnementController extends BaseController
@@ -19,7 +21,7 @@ class ApprovisionnementController extends BaseController
      */
     public function index()
     {
-        //
+        return view('stock.product.historic_approvision');
     }
 
     /**
@@ -41,9 +43,31 @@ class ApprovisionnementController extends BaseController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ApprovisionnementRequest $request)
     {
-        //
+        $request->validated();
+        $data = $request->all();
+        $data['date_approvision'] = date("Y-m-d", strtotime($data['date_approvision']));
+        if($produit = Produit::findOrFail($data['produit'])){
+
+            //On sauvegarde les details de l'approvisionnement
+            $approvision = new Approvisionnement;
+            $approvision->num_BL = $data['num_bl'];
+            $approvision->produit = $data['produit'];
+            $approvision->fournisseur = $data['fournisseur'];
+            $approvision->conditionnement = $data['conditionnement'];
+            $approvision->qte_cond = $data['qte_par_cond'];
+            $approvision->total = $data['total'];
+            $approvision->date_appr = $data['date_approvision'];
+            $approvision->save();
+            //On augmente le stock actuel
+            $produit->qte_stock+=$data['total'];
+            $produit->save();
+            echo "success";
+        }
+
+        die;
+
     }
 
     /**
@@ -89,5 +113,37 @@ class ApprovisionnementController extends BaseController
     public function destroy($id)
     {
         //
+    }
+
+    public function liste_appro_date(Request $request){
+        $data = $request->all();
+        $response = '<tr><td>Aucun resultat</td></tr>';
+        $date_appr = $data['date_appr'];
+        //On verifie s'il existe des approvisionnements a la date choisis par l'utilisateur
+        if($approvisionnements = Approvisionnement::whereDate('date_appr', '=', $data['date_appr'])->get()){
+            $response = '';
+            foreach ($approvisionnements as $approvision) {
+
+                //On recupere le nom du produit grace a l'id correspondant au produit
+                if($produit = Produit::findOrFail($approvision->produit)){
+                    $approvision->nom_prod = $produit->nom_produit;
+                }
+                //On recupere le nom du fournisseur grace a l'id correspondant au nom du fournisseur
+                if($fournisseur = Fournisseur::findOrFail($approvision->fournisseur)){
+                    $approvision->nom_fournisseur = $fournisseur->nom_fournisseur;
+                }
+                $response.='
+                            <tr>
+                                <td>'.$approvision->num_BL.'</td>
+                                <td>'.$approvision->nom_prod.'</td>
+                                <td>'.$approvision->conditionnement.'</td>
+                                <td>'.$approvision->qte_cond.'</td>
+                                <td>'.$approvision->total.'</td>
+                                <td>'.$approvision->nom_fournisseur.'<td>
+                            </tr>
+                ';
+            }
+        }
+        return $response;
     }
 }
