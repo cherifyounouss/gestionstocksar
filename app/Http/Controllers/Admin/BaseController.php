85441d0f42ad775abc0d2cac6e2ccdd84b2095d3
rifmeister <chinsanity16@gmail.com>
2018-10-03 16:50:48 +0000

    controller: BaseController updated

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Etagere;
 use App\Produit;
 use View;
+use DateTime;
+use Carbon\Carbon;
 
 class BaseController extends Controller
 {
@@ -15,20 +17,32 @@ class BaseController extends Controller
       $produits = Produit::all();
       //tableau pour stocker les produits en voie de rupture
       $produits_en_rupt = array();
+      //tableau pour stocker les produits en voie de peremption
+      $produits_en_per = array();
+      //Date du jour
+      $today = Carbon::now();
       foreach ($produits as $produit) {
           //Si la quantite minimale accepté pour un produit est superieur a la quantite disponible en stock
           //Alors on conserve le dit produit au niveau de la table $produits_en_rupt
           if($produit->qte_min >= $produit->qte_stock){
               $produits_en_rupt[] = $produit;
           }
+          $date_peremption = Carbon::createFromFormat('Y-m-d', $produit->date_peremption);
+          //Les produits dont la date de peremption est dans deux semaines
+          if ($date_peremption->diffInWeeks($today) <= 2){
+              $produit->intervalle = $date_peremption->diffInWeeks($today);
+              $produits_en_per[] = $produit;
+          }
       }
-
-      $nb_alertes = count($produits_en_rupt);
+      //nombres d'alertes total = nombre d'alertes pour rupture de stock + nombre d'alerte pour date de preemption
+      $nb_alertes = count($produits_en_per) + count($produits_en_rupt);
 
       //its just a dummy data object.
       $etageres = Etagere::all();
-      
+      //nombres d'alertes partagées avec toutes les vues
       View::share('nb_alertes', $nb_alertes);
+      View::share('nb_produits_en_per', count($produits_en_per));
+      View::share('nb_produits_en_rupt', count($produits_en_rupt));
       // Sharing is caring
       View::share('etageres', $etageres);
     }
