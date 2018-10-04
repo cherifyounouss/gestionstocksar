7dcaadec3b9d5cbb422d17c97592d96c70718b61
rifmeister <chinsanity16@gmail.com>
2018-10-04 16:31:32 +0000

    vue: historique des approvisionnements updated

--- resources/views/stock/product/historic_approvision.blade.php
+++ resources/views/stock/product/historic_approvision.blade.php
@@ -6,12 +6,12 @@
 <div class="page-breadcrumb">
     <div class="row">
         <div class="col-12 d-flex no-block align-items-center">
-            <h4 class="page-title">Fournisseurs</h4>
+            <h4 class="page-title">Historique approvisionnements</h4>
             <div class="ml-auto text-right">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{url('/dashboard')}}">Accueil</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Liste des fournisseurs</li>
+                        <li class="breadcrumb-item active" aria-current="page">Historique approvisionnement</li>
                     </ol>
                 </nav>
             </div>
