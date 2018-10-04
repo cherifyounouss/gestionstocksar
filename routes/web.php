6807fc1da218ba49f5fa04c4cc0ded446f824a25
rifmeister <chinsanity16@gmail.com>
2018-10-04 16:31:58 +0000

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -11,66 +11,94 @@
 |
 */
 
-Route::get('/','Admin\EtagereController@listeEtagere');
-Route::get('/dashboard', 'Admin\EtagereController@listeEtagere');
+// Route::get('/','Admin\EtagereController@listeEtagere');
+// Route::get('/dashboard', 'Admin\EtagereController@listeEtagere');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/se_connecter','Admin\LoginController@showLoginForm');
-Route::post('/connexion','Admin\LoginController@login');
-Route::post('Admin\LoginController@logout');
-Route::get('utilisateur/reset/{token}', 'Admin\ResetPasswordController@showResetForm');
-Route::post('utilisateur/reset', 'Admin\ResetPasswordController@reset');
-Route::get('/accueil', function() {
-    return "Bienvenue sur la page d'accueil";
+Route::group(['middleware' => 'utilisateur_non_connecte'], function() {
+
+    Route::get('/dashboard', 'Admin\HomeController@home');
+    Route::get('/', 'Admin\HomeController@home');
+
+    //Fournisseur
+    Route::get('/ajouter_fournisseur', 'Admin\ProviderController@create');
+    Route::post('/ajouter_fournisseur', 'Admin\ProviderController@store');
+    Route::get('/liste_fournisseur', 'Admin\ProviderController@index');
+    Route::get('/modifier_fournisseur/{id}', 'Admin\ProviderController@edit');
+    Route::post('/modifier_fournisseur/{id}', 'Admin\ProviderController@update');
+    Route::post('/supprimer_fournisseur','Admin\ProviderController@destroy');
+
+    //Utilisateur
+    Route::get('/ajouter_utilisateur', 'Admin\UserController@create');
+    Route::post('/ajouter_utilisateur', 'Admin\UserController@store');
+    Route::get('/liste_utilisateur', 'Admin\UserController@index');
+    Route::post('/supprimer_utilisateur', 'Admin\UserController@destroy');
+
+    //Etagere
+    Route::get('/stock/ajouter_etagere', 'Admin\EtagereController@create');
+    Route::post('/stock/ajouter_etagere', 'Admin\EtagereController@store');
+    Route::get('/stock/liste_etagere', 'Admin\EtagereController@index');
+    Route::get('/stock/modifier_etagere/{id}', 'Admin\EtagereController@edit');
+    Route::post('/stock/modifier_etagere/{id}', 'Admin\EtagereController@update');
+    Route::post('/stock/supprimer_etagere', 'Admin\EtagereController@destroy');
+
+    //Produit
+    Route::get('/stock/ajouter_produit', 'Admin\ProductController@create');
+    Route::post('/stock/ajouter_produit', 'Admin\ProductController@store');
+    Route::get('/stock/liste_produit', 'Admin\ProductController@index');
+    Route::get('/view_file/{fds}','Admin\ProductController@view_file');
+    Route::get('/stock/modifier_produit/{id}', 'Admin\ProductController@edit');
+    Route::post('/stock/modifier_produit/{id}', 'Admin\ProductController@update');
+    Route::post('/stock/supprimer_produit', 'Admin\ProductController@destroy');
+
+    //Notifications d'alerte
+    Route::get('/notifications/alerte_stock', 'Admin\ProductController@get_alerte_stock');
+    Route::get('/notifications/alerte_peremption', 'Admin\ProductController@get_alerte_date_prmtion');
+
+    //Unite
+    Route::post('/stock/ajouter_unite', 'Admin\UniteController@store');
+    Route::get('/stock/unites', 'Admin\UniteController@unite_ajax');
+
+    Route::get('/stock/liste_produit_etagere/{id}', 'Admin\ProductController@index_etagere');
+
+    //Approvisionnement
+    Route::get('/stock/approvisionner', 'Admin\ApprovisionnementController@create');
+    Route::post('/stock/approvisionner', 'Admin\ApprovisionnementController@store');
+    Route::get('/stock/historique_approvisionnement', 'Admin\ApprovisionnementController@index');
+    Route::get('/liste_approvisionnement', 'Admin\ApprovisionnementController@liste_appro_date');
+
+    //Consommation
+    Route::get('/stock/consommer', 'Admin\ConsommationController@create');
+    Route::post('/stock/consommer', 'Admin\ConsommationController@store');
+    Route::get('/stock/historique_consommation', 'Admin\ConsommationController@index');
+    Route::get('/liste_consommation', 'Admin\ConsommationController@liste_cons_date');
+
+    Route::post('/deconnexion', 'Admin\LoginController@logout');
 });
-Route::post('/password_request','Admin\ForgotPasswordController@sendResetLinkEmail');
 
-//Fournisseur
-Route::get('/ajouter_fournisseur', 'Admin\ProviderController@create');
-Route::post('/ajouter_fournisseur', 'Admin\ProviderController@store');
-Route::get('/liste_fournisseur', 'Admin\ProviderController@index');
-Route::get('/modifier_fournisseur/{id}', 'Admin\ProviderController@edit');
-Route::post('/modifier_fournisseur/{id}', 'Admin\ProviderController@update');
-Route::post('/supprimer_fournisseur','Admin\ProviderController@destroy');
+Route::group(['middleware' => 'utilisateur_connecte'], function() {
+    Route::get('/se_connecter','Admin\LoginController@showLoginForm');
+    Route::post('/connexion','Admin\LoginController@login');
+    Route::get('utilisateur/reset/{token}', 'Admin\ResetPasswordController@showResetForm');
+    Route::post('utilisateur/reset', 'Admin\ResetPasswordController@reset');
+    Route::post('/password_request','Admin\ForgotPasswordController@sendResetLinkEmail');
+    
+});
 
-//Utilisateur
-Route::get('/ajouter_utilisateur', 'Admin\UserController@create');
-Route::post('/ajouter_utilisateur', 'Admin\UserController@store');
-Route::get('/liste_utilisateur', 'Admin\UserController@index');
-Route::post('/supprimer_utilisateur', 'Admin\UserController@destroy');
 
-//Etagere
-Route::get('/stock/ajouter_etagere', 'Admin\EtagereController@create');
-Route::post('/stock/ajouter_etagere', 'Admin\EtagereController@store');
-Route::get('/stock/liste_etagere', 'Admin\EtagereController@index');
-Route::get('/stock/modifier_etagere/{id}', 'Admin\EtagereController@edit');
-Route::post('/stock/modifier_etagere/{id}', 'Admin\EtagereController@update');
-Route::post('/stock/supprimer_etagere', 'Admin\EtagereController@destroy');
 
-//Produit
-Route::get('/stock/ajouter_produit', 'Admin\ProductController@create');
-Route::post('/stock/ajouter_produit', 'Admin\ProductController@store');
-Route::get('/stock/liste_produit', 'Admin\ProductController@index');
-Route::get('/view_file/{fds}','Admin\ProductController@view_file');
-Route::get('/stock/modifier_produit/{id}', 'Admin\ProductController@edit');
-Route::post('/stock/modifier_produit/{id}', 'Admin\ProductController@update');
-Route::post('/stock/supprimer_produit', 'Admin\ProductController@destroy');
 
-//Unite
-Route::post('/stock/ajouter_unite', 'Admin\UniteController@store');
-Route::get('/stock/unites', 'Admin\UniteController@unite_ajax');
 
-Route::get('/stock/liste_produit_etagere/{id}', 'Admin\ProductController@index_etagere');
 
-//Approvisionnement
-Route::get('/stock/approvisionner', 'Admin\ApprovisionnementController@create');
-Route::post('/stock/approvisionner', 'Admin\ApprovisionnementController@store');
-Route::get('/stock/historique_approvisionnement', 'Admin\ApprovisionnementController@index');
-Route::get('/liste_approvisionnement', 'Admin\ApprovisionnementController@liste_appro_date');
 
-//Notifications d'alerte
-Route::get('/notifications/alerte_stock', 'Admin\ProductController@get_alerte_stock');
-Route::get('/notifications/alerte_peremption', 'Admin\ProductController@get_alerte_date_prmtion');
+
+
+
+
+
+
+
+
+
