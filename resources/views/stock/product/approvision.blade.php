c3a4e9dfa7af23742eaeffb11bb69a66e5d80291
rifmeister <chinsanity16@gmail.com>
2018-10-08 16:43:02 +0000

    view: approvisionner stock

--- resources/views/stock/product/approvision.blade.php
+++ resources/views/stock/product/approvision.blade.php
@@ -22,6 +22,12 @@
 @endsection
 @section('content')
 <div class="card">
+        @if(Session::has('successMessage'))
+        <div class="alert alert-success" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>        
+            {{session('successMessage')}}
+        </div>        
+        @endif
         <form class="form-horizontal" method="POST" action="{{url('/stock/approvisionner')}}">
             {{csrf_field()}}
             <div class="card-body">
