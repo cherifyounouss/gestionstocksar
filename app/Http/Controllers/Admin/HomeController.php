fa2087ca5d86679edafd265f7489f7a456198ac3
rifmeister <chinsanity16@gmail.com>
2018-10-10 16:45:34 +0000

    controller: HomeController updated

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,10 +5,24 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Admin\BaseController;
+use App\Produit;
+use App\Utilisateur;
+use App\Etagere;
+use App\Fournisseur;
+
 
 class HomeController extends BaseController
 {
     public function home() {
-        return view('dashboard');
+        $nb_produits = Produit::all()->count();
+        $nb_utilisateurs = Utilisateur::all()->count();
+        $nb_etageres = Etagere::all()->count();
+        $nb_fournisseurs = Fournisseur::all()->count();
+        return view('dashboard')->with([
+            'nb_produits' => $nb_produits,
+            'nb_utilisateurs' => $nb_utilisateurs,
+            'nb_etageres' => $nb_etageres,
+            'nb_fournisseurs' => $nb_fournisseurs,
+        ]);
     }
 }
