3bf7f311bef34bda1ecdbe48ad152fa20d1ad5d2
rifmeister <chinsanity16@gmail.com>
2018-10-10 16:46:55 +0000

    view: dashboard updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,57 @@
 @extends('layouts.layout')
+@section('crumb')
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-12 d-flex no-block align-items-center">
+            <h4 class="page-title">Accueil</h4>
+            <div class="ml-auto text-right">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('content')
+<div class="row">
+    <!-- Column -->
+    <div class="col-md-6 col-lg-3">
+        <div class="card card-hover">
+            <div class="box bg-cyan text-center">
+                <h1 class="font-light text-white">{{ $nb_produits}}</h1>
+                <h6 class="text-white">Produits</h6>
+            </div>
+        </div>
+    </div>
+    <!-- Column -->
+    <div class="col-md-6 col-lg-3">
+        <div class="card card-hover">
+            <div class="box bg-success text-center">
+                <h1 class="font-light text-white">{{ $nb_utilisateurs}}</h1>
+                <h6 class="text-white">Utilisateurs</h6>
+            </div>
+        </div>
+    </div>
+     <!-- Column -->
+    <div class="col-md-6 col-lg-3">
+        <div class="card card-hover">
+            <div class="box bg-warning text-center">
+                <h1 class="font-light text-white">{{ $nb_etageres}}</h1>
+                <h6 class="text-white">&Eacute;tag&egrave;res</h6>
+            </div>
+        </div>
+    </div>
+    <!-- Column -->
+    <div class="col-md-6 col-lg-3">
+        <div class="card card-hover">
+            <div class="box bg-danger text-center">
+                <h1 class="font-light text-white">{{ $nb_fournisseurs}}</h1>
+                <h6 class="text-white">Fournisseurs</h6>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
